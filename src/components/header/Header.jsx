028c0640db1327c751c51a116af9aaf21848090e
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-09-27 18:57:50 +0500

    Add component PopUser

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,6 @@
+import { PopUser } from "../popups/popUser/PopUser";
+
+
 export const Header = () => {
   return (
     <header class="header">
@@ -12,16 +15,7 @@ export const Header = () => {
           <nav class="header__nav">
             <button class="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
             <a href="#user-set-target" class="header__user _hover02">Ivan Ivanov</a>
-            <div class="header__pop-user-set pop-user-set" id="user-set-target">
-              {/* <!-- <a href="">x</a> --> */}
-              <p class="pop-user-set__name">Ivan Ivanov</p>
-              <p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-              <div class="pop-user-set__theme">
-                <p>Темная тема</p>
-                <input type="checkbox" class="checkbox" name="checkbox" />
-              </div>
-              <button type="button" class="_hover03"><a href="#popExit">Выйти</a></button>
-            </div>
+            <PopUser />
           </nav>
         </div>
       </div>
