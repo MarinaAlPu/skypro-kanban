f8db0e3f010907fde0ed543cad487beaf3c3c8fe
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-10-15 23:12:51 +0500

    Fix styled components

--- src/components/main/Main.styled.js
+++ src/components/main/Main.styled.js
@@ -1,57 +1,25 @@
 import styled from "styled-components";
 
-// export const SMain = styled.main`
-//   .main {
-//     width: 100%;
-//     background-color: #EAEEF6;
-//   }
-
-//   .container {
-//     max-width: 1260px;
-//     width: 100%;
-//     margin: 0 auto;
-//     padding: 0 30px;
-//   }
-
-//   .main__block {
-//     width: 100%;
-//     margin: 0 auto;
-//     padding: 25px 0 49px;
-//   }
-
-//   .main__content {
-//     width: 100%;
-//     display: flex;
-// }
-// `
 
 export const SMain = styled.main`
-  .main {
     width: 100%;
     background-color: #EAEEF6;
-  }
 `
 
 export const SContainer = styled.div`
-  .container {
     max-width: 1260px;
     width: 100%;
     margin: 0 auto;
     padding: 0 30px;
-  }
 `
 
 export const SBlock = styled.div`
-  .main__block {
     width: 100%;
     margin: 0 auto;
     padding: 25px 0 49px;
-  }
 `
 
 export const SContent = styled.div`
-  .main__content {
     width: 100%;
     display: flex;
-}
 `
