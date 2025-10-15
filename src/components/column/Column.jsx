36d368f049fd0b51f2ca3b41339537bc72ee53de
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-10-15 23:08:56 +0500

    Add styled components in Column

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -1,27 +1,32 @@
 import { Card } from "../card/Card";
+import { SColumn, SColumnTitleContainer, SColumnTitle, SCards, SCardItem } from "./Column.styled";
 
 
 export const Column = ({ title, cardsByStatus }) => {
   return (
-    <div className="main__column column">
-      <div className="column__title">
-        <p>{title}</p>
-      </div>
-      <div className="cards">
+    // <div className="main__column column">
+    <SColumn>
+      {/* <div className="column__title"> */}
+      <SColumnTitleContainer>
+        <SColumnTitle>{title}</SColumnTitle>
+      </SColumnTitleContainer>
+      {/* <div className="cards"> */}
+      <SCards>
         {
           cardsByStatus[title].map((card) => {
             return (
-              <div className="cards__item" key={card.id}>
+              // <div className="cards__item" key={card.id}>
+              <SCardItem key={card.id}>
                 <Card
                   topic={card.topic}
                   title={card.title}
                   date={card.date}
                 />
-              </div>
+              </SCardItem>
             )
           })
         }
-      </div>
-    </div>
+      </SCards>
+    </SColumn>
   )
 }
--- src/components/column/Column.styled.js
+++ src/components/column/Column.styled.js
@@ -1,3 +1,49 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+
+export const SColumn = styled.div`
+  width: 20%;
+  margin: 0 auto;
+  display: block;
+`
+
+export const SColumnTitleContainer = styled.div`
+  padding: 0 10px;
+  margin: 15px 0;
+`
+
+export const SColumnTitle = styled.p`
+  color: #94A6BE;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+  text-transform: uppercase;
+`
+
+export const SCards = styled.div`
+  width: 100%;
+  display: block;
+  position: relative;
+`
+
+const cardAnimation = keyframes`
+  0% {
+    height: 0;
+    opacity: 0;
+  }
+
+  100% {
+    height: auto;
+    opacity: 1;
+  }
+`
+
+export const SCardItem = styled.div`
+  padding: 5px;
+  animation-name: ${cardAnimation};
+  animation-duration: 500ms;
+  animation-timing-function: linear;
+`
+
 
 
