f933ac02c63e3e674cf2365da398fe6a646d8c23
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-10-26 14:16:14 +0500

    Fix button styles

--- src/components/button/Button.styled.js
+++ src/components/button/Button.styled.js
@@ -15,16 +15,17 @@ export const SButton = styled.button`
   border-radius: 4px;
   font-size: 14px;
   font-weight: 500;
-  color: "#FFFFFF";
+  color: #FFFFFF;
   background-color: ${({$type}) => colors[$type]};
   ${({$type}) => ($type === "secondary" ? "color: #565EEF" : "")}
 `;
 
 export const PrimaryButton = styled(SButton)`
-  background-color: "#565EEF";
+  background-color: #565EEF;
 `;
 
 export const SecondaryButton = styled(SButton)`
-  background-color: "#FFFFFF";
+  background-color: #FFFFFF;
   color: #565EEF;
+  border: 0.7px solid #565EEF;
 `;
