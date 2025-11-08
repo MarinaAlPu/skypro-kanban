d1fca43e35d7031447d04acb587379f025fddb18
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-11-08 22:06:32 +0500

    Add api functions

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -3,7 +3,7 @@ import { Column } from "../column/Column";
 import { SContent, SContainer, SBlock, SData } from "./Content.styled";
 
 
-export const Content = ({ cards }) => {
+export const Content = ({ cards, error, tasks }) => {
   const cardsByStatus = statuses.reduce((acc, status) => {
     acc[status] = cards.filter((card) => card.status === status);
     return acc;
@@ -19,6 +19,7 @@ export const Content = ({ cards }) => {
           </SData>
         </SBlock>
       </SContainer>
+      <p>{error}</p> // вывод ошибки
     </SContent>
   )
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,8 @@
 import { Main } from "../components/main/Main";
 import { Outlet } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { Loader } from "../components/loader/Loader";
+import { fetchTasks } from "../services/api";
 
 
 export const MainPage = ({ setIsAuth, cards }) => {
@@ -11,12 +12,34 @@ export const MainPage = ({ setIsAuth, cards }) => {
     setTimeout(() => {
       setIsLoading(false)
     }, 1000);
-  }, [])
+  }, []);
+
+
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState("");
+
+  const getTasks = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const data = await fetchTasks({
+        token: "bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck",
+      });
+      if (data) setTasks(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getTasks();
+  }, [getTasks]);
 
   return (
     <>
       {isLoading && <Loader isLoading={isLoading} />}
-      <Main setIsAuth={setIsAuth} cards={cards} />
+      <Main setIsAuth={setIsAuth} cards={cards} error={error} tasks={tasks} />
       <Outlet />
     </>
   )
