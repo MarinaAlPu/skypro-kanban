6aa78ac3da5a5593f5462af9a75ac9b69c580955
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-11-19 22:02:47 +0500

    Add file helpers.js, move function validateForm in helpers,js

--- src/components/authForm/AuthForm.jsx
+++ src/components/authForm/AuthForm.jsx
@@ -4,7 +4,7 @@ import { Button } from "../button/Button";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { login, registration } from "../../services/auth";
-// import { validateForm } from "../../utils/helpers";
+import { validateForm } from "../../utils/helpers";
 
 
 export const AuthForm = ({ isSignUp, setIsAuth }) => {
@@ -16,56 +16,58 @@ export const AuthForm = ({ isSignUp, setIsAuth }) => {
   const [isValid, setIsValid] = useState(true);
 
 
-  const validateForm = () => {
-    const newErrors = { name: "", login: "", password: "" };
+  // const validateForm = () => {
+  //   const newErrors = { name: "", login: "", password: "" };
 
-    // ошибки при регистрации (пустые поля)
-    if (isSignUp && !formData.name.trim()) {
-      newErrors.name = true;
-      setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, заполните все поля в форме.");
-      setIsValid(false);
-    } else if (isSignUp && !formData.login.trim()) {
-      newErrors.login = true;
-      setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, заполните все поля в форме.");
-      setIsValid(false);
-    } else if (isSignUp && !formData.password.trim()) {
-      newErrors.password = true;
-      setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, заполните все поля в форме.");
-      setIsValid(false);
-    }
+  //   // ошибки при регистрации (пустые поля)
+  //   if (isSignUp && !formData.name.trim()) {
+  //     newErrors.name = true;
+  //     setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, заполните все поля в форме.");
+  //     setIsValid(false);
+  //   } else if (isSignUp && !formData.login.trim()) {
+  //     newErrors.login = true;
+  //     setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, заполните все поля в форме.");
+  //     setIsValid(false);
+  //   } else if (isSignUp && !formData.password.trim()) {
+  //     newErrors.password = true;
+  //     setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, заполните все поля в форме.");
+  //     setIsValid(false);
+  //   }
 
-    // ошибки при регистрации (меньше 3 символов)
-    if (isSignUp && formData.name.length < 3) {
-      newErrors.name = true;
-      setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, введите данные корректно и повторите попытку.");
-      setIsValid(false);
-    } else if (isSignUp && formData.login.length < 3) {
-      newErrors.login = true;
-      setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, введите данные корректно и повторите попытку.");
-      setIsValid(false);
-    } else if (isSignUp && formData.password.length < 3) {
-      newErrors.password = true;
-      setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, введите данные корректно и повторите попытку.");
-      setIsValid(false);
-    }
+  //   // ошибки при регистрации (меньше 3 символов)
+  //   if (isSignUp && formData.name.length < 3) {
+  //     newErrors.name = true;
+  //     setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, введите данные корректно и повторите попытку.");
+  //     setIsValid(false);
+  //   } else if (isSignUp && formData.login.length < 3) {
+  //     newErrors.login = true;
+  //     setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, введите данные корректно и повторите попытку.");
+  //     setIsValid(false);
+  //   } else if (isSignUp && formData.password.length < 3) {
+  //     newErrors.password = true;
+  //     setError("Введенные вами данные не корректны. Чтобы завершить регистрацию, введите данные корректно и повторите попытку.");
+  //     setIsValid(false);
+  //   }
 
-    // ошибки при входе (пустые поля)
-    if (!isSignUp && !formData.login.trim()) {
-      newErrors.login = true;
-      setError("Введенные вами данные не распознаны. Проверьте свой логин и пароль и повторите попытку входа.");
-      setIsValid(false);
-    } else if (!isSignUp && !formData.password.trim()) {
-      newErrors.password = true;
-      setError("Введенные вами данные не распознаны. Проверьте свой логин и пароль и повторите попытку входа.");
-      setIsValid(false);
-    }
+  //   // ошибки при входе (пустые поля)
+  //   if (!isSignUp && !formData.login.trim()) {
+  //     newErrors.login = true;
+  //     setError("Введенные вами данные не распознаны. Проверьте свой логин и пароль и повторите попытку входа.");
+  //     setIsValid(false);
+  //   } else if (!isSignUp && !formData.password.trim()) {
+  //     newErrors.password = true;
+  //     setError("Введенные вами данные не распознаны. Проверьте свой логин и пароль и повторите попытку входа.");
+  //     setIsValid(false);
+  //   }
 
-    setErrors(newErrors);
-    return isValid;
-  }
+  //   setErrors(newErrors);
+  //   return isValid;
+  //   // return error;
+  // }
 
   const handleChange = (e) => {
     setIsValid(true);
+    // setError("");
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
     setErrors({ ...errors, [name]: false });
@@ -74,14 +76,17 @@ export const AuthForm = ({ isSignUp, setIsAuth }) => {
 
   const handleLogin = async (e) => {
     setIsValid(false);
+    // setError("");
 
     e.preventDefault();
-    if (!validateForm()) {
+
+    if (!validateForm(formData, isSignUp, setErrors, setError, setIsValid)) {
       return;
     }
+
     try {
       const data = !isSignUp ? await login({ login: formData.login, password: formData.password }) : await registration(formData);
-      
+
       if (data) {
         setIsAuth(true);
         localStorage.setItem("userInfo", JSON.stringify(data));
@@ -107,6 +112,7 @@ export const AuthForm = ({ isSignUp, setIsAuth }) => {
 
           <SErrorMessageWrapper><SErrorMessageText>{error}</SErrorMessageText></SErrorMessageWrapper>
 
+          {/* <Button text={isSignUp ? "Зарегистрироваться" : "Войти"} type="primary" disabled={error} /> */}
           <Button text={isSignUp ? "Зарегистрироваться" : "Войти"} type="primary" disabled={!isValid} />
 
           {!isSignUp && (
