32919bcf747521e44b6103565d200b28096c2fff
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-11-25 11:00:03 +0500

    Add comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 
 function App() {
-  // опциональная цепочка - получить token, если есть значение перед ?., иначе undefined
+  // опциональная цепочка - получить token, если есть значение перед ?., иначе undefined, а не ошибка
   const [token, setToken] = useState(JSON.parse(localStorage.getItem("userInfo"))?.token);
 
   return <AppRoutes token={token} setToken={setToken} />
