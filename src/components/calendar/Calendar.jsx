aa93ab5db559512a73edc15f4c6bce77ac232a8c
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-02-20 10:52:00 +0700

    Add setting month and year to display in calendar. Fix displaing month and year names in calendar

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -5,38 +5,53 @@ import { SCalendarContainer, SCalendarTitle, SCalendarBlock, SCalendarNavigation
 export const Calendar = ({ isEditTask }) => {
   // console.log("isEditTask: ", isEditTask);
 
-    const currentDate = new Date();
+  // const currentDateForTitle = new Date();
 
-  // const [currentDate, setCurrentDate] = useState(new Date());
-  // const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-  // const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  // let currentMonthName = currentDateForTitle.toLocaleString('ru-RU', { month: 'long' });
+  // currentMonthName = currentMonthName.charAt(0).toUpperCase() + currentMonthName.slice(1);
+  // let currentMonthYear = currentMonthName + " " + currentDateForTitle.getFullYear();
+  
+  
+  // const [currentDateToDisplay, setCurrentDateToDisplay] = useState(new Date());
+  const [currentMonthToDisplay, setCurrentMonthToDisplay] = useState(new Date().getMonth());
+  const [currentYearToDisplay, setCurrentYearToDisplay] = useState(new Date().getFullYear());
+  // const [currentMonthToDisplay, setCurrentMonthToDisplay] = useState("");
+  // const [currentYearToDisplay, setCurrentYearToDisplay] = useState("");
+  
+  const currentDateToDisplay = new Date(currentYearToDisplay, currentMonthToDisplay);
+  // console.log("currentDateToDisplay: ", currentDateToDisplay);
+  
+  const currentMonthName = currentDateToDisplay.toLocaleString('ru-RU', { month: 'long' });
+  // console.log("currentMonthName: ", currentMonthName);
+  
+  const currentMonthNameForTitle = currentMonthName.charAt(0).toUpperCase() + currentMonthName.slice(1);
+  // console.log("currentMonthNameForTitle: ", currentMonthNameForTitle);
 
-  // console.log("currentDate: ", currentDate);
-
-  // const currentYear1 = currentDate.getFullYear();
-  // console.log("currentYear1: ", currentYear1);
-  // let currentMonth1 = Number(currentDate.getMonth()) + 1;
-  // console.log("currentMonth1: ", currentMonth1);
-  // if (currentMonth1 < 10) {
-  //   currentMonth1 = 0 + String(currentMonth1);
-  //   console.log("currentMonth1: ", currentMonth1);
-  // }
-
-  let currentMonthName = currentDate.toLocaleString('ru-RU', { month: 'long' });
-  currentMonthName = currentMonthName.charAt(0).toUpperCase() + currentMonthName.slice(1);
-  const currentMonthYear = currentMonthName + " " + currentDate.getFullYear();
-
-
-  // let currentDate1 = currentDate.getDate();
-  // console.log("currentDay1: ", currentDate1);
-  // if (currentDate1 < 10) {
-  //   currentDate1 = 0 + String(currentDate1);
-  //   console.log("currentDate1: ", currentDate1);
-  // }
+  const currentPeriodToDisplay = currentMonthNameForTitle + " " + currentYearToDisplay;
+  // console.log("currentPeriodToDisplay: ", currentPeriodToDisplay);
 
 
-  // const currentDateForTask = currentDate1 + "." + currentMonth1 + "." + currentYear1
-  // console.log("currentDateForTask: ", currentDateForTask);
+  const daysInCurrentMonthToDisplay = new Date(currentYearToDisplay, currentMonthToDisplay + 1, 0).getDate();
+  console.log("daysInCurrentMonth: ", daysInCurrentMonthToDisplay);
+
+
+  const onSelectPrevMonth = () => {
+    if (currentMonthToDisplay === 0) {
+      setCurrentMonthToDisplay(11);
+      setCurrentYearToDisplay(currentYearToDisplay - 1);
+    } else {
+      setCurrentMonthToDisplay(currentMonthToDisplay - 1);
+    }
+  };
+
+  const onSelectNextMonth = () => {
+    if (currentMonthToDisplay === 11) {
+      setCurrentMonthToDisplay(0);
+      setCurrentYearToDisplay(currentYearToDisplay + 1);
+    } else {
+      setCurrentMonthToDisplay(currentMonthToDisplay + 1);
+    }
+  };
 
 
   return (
@@ -44,14 +59,20 @@ export const Calendar = ({ isEditTask }) => {
       <SCalendarTitle>Даты</SCalendarTitle>
       <SCalendarBlock>
         <SCalendarNavigation>
-          <SCalendarMonth>{currentMonthYear}</SCalendarMonth>
+          <SCalendarMonth>{currentPeriodToDisplay}</SCalendarMonth>
           <SCalendarNavActions>
-            <SCalendarNavAction data-action="prev">
+            <SCalendarNavAction
+              data-action="prev"
+              onClick={onSelectPrevMonth}
+            >
               <SCalendarArrow xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11">
                 <path d="M5.72945 1.95273C6.09018 1.62041 6.09018 1.0833 5.72945 0.750969C5.36622 0.416344 4.7754 0.416344 4.41218 0.750969L0.528487 4.32883C-0.176162 4.97799 -0.176162 6.02201 0.528487 6.67117L4.41217 10.249C4.7754 10.5837 5.36622 10.5837 5.72945 10.249C6.09018 9.9167 6.09018 9.37959 5.72945 9.04727L1.87897 5.5L5.72945 1.95273Z" />
               </SCalendarArrow>
             </SCalendarNavAction>
-            <SCalendarNavAction data-action="next">
+            <SCalendarNavAction
+              data-action="next"
+              onClick={onSelectNextMonth}
+            >
               <SCalendarArrow xmlns="http://www.w3.org/2000/svg" width="6" height="11" viewBox="0 0 6 11">
                 <path d="M0.27055 9.04727C-0.0901833 9.37959 -0.0901832 9.9167 0.27055 10.249C0.633779 10.5837 1.2246 10.5837 1.58783 10.249L5.47151 6.67117C6.17616 6.02201 6.17616 4.97799 5.47151 4.32883L1.58782 0.75097C1.2246 0.416344 0.633778 0.416344 0.270549 0.75097C-0.0901831 1.0833 -0.090184 1.62041 0.270549 1.95273L4.12103 5.5L0.27055 9.04727Z" />
               </SCalendarArrow>
