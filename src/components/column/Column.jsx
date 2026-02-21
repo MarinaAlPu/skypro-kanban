9959f42ac337d5e8a1c145d29292e01ef5685cd3
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-02-21 15:33:14 +0700

    Add new loader

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
 import { Card } from "../card/Card";
+import { Loader } from "../loader/Loader";
 import { SColumn, SColumnTitleContainer, SColumnTitle, SCards, SCardItem } from "./Column.styled";
 import { format } from "date-fns";
+import { TasksContext } from "../../context/TasksContext";
 
 
 export const Column = ({ title, cardsByStatus }) => {
+  const { isLoading } = useContext(TasksContext);
+  // console.log("isLoading: ", isLoading);
+
+
   return (
     <SColumn>
       <SColumnTitleContainer>
@@ -11,18 +18,26 @@ export const Column = ({ title, cardsByStatus }) => {
       </SColumnTitleContainer>
       <SCards>
         {
-          cardsByStatus[title].map((card) => {
-            return (
-              <SCardItem key={card._id}>
-                <Card
-                  id={card._id}
-                  topic={card.topic}
-                  title={card.title}
-                  date={format(card.date, "dd.MM.yy")}
-                />
-              </SCardItem>
+          isLoading
+            ?
+            (
+              <Loader />
             )
-          })
+            :
+            (
+              cardsByStatus[title].map((card) => {
+                return (
+                  <SCardItem key={card._id}>
+                    <Card
+                      id={card._id}
+                      topic={card.topic}
+                      title={card.title}
+                      date={format(card.date, "dd.MM.yy")}
+                    />
+                  </SCardItem>
+                )
+              })
+            )
         }
       </SCards>
     </SColumn>
